476f25f0b285b07f7a693e269a7e3e9f2dbe877d
Fez06 <mattiafezzardi1993@gmail.com>
2023-04-05 17:43:44 +0200

    dati messi a tabella

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>php-hotel</title>
 </head>
+
 <body>
     <?php
     $hotels = [
@@ -52,16 +54,31 @@
 
     ?>
 
-    
-    <?php foreach($hotels as $hotel) { ?>
-        <ul>
-            <?php foreach($hotel as $dato) { ?>
-            <li><?php echo $dato ?></li>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Hotel</th>
+                <th scope="col">Hotel description</th>
+                <th scope="col">Parking?</th>
+                <th scope="col">Vote</th>
+                <th scope="col">Distance to center (km)</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+            <?php foreach ($hotels as $hotel) { ?>
+                <tr>
+                    <?php foreach ($hotel as $dato) { ?>
+                        <td><?php echo $dato ?></td>
+                    <?php } ?>
+                </tr>
             <?php } ?>
-        </ul>
-        <?php } ?>
-        
+        </tbody>
+
+    </table>
+
     <!-- bootstrap script -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
+
 </html>
