2faad1c55176deccb88c696e4d2ae395f5bd0e87
Fez06 <mattiafezzardi1993@gmail.com>
2023-04-05 17:15:07 +0200

    dati stampati in pagina con ul ed li

--- index.php
+++ index.php
@@ -7,6 +7,58 @@
     <title>php-hotel</title>
 </head>
 <body>
+    <?php
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+    ?>
+
+    
+    <?php foreach($hotels as $hotel) { ?>
+        <ul>
+            <?php foreach($hotel as $dato) { ?>
+            <li><?php echo $dato ?></li>
+            <?php } ?>
+        </ul>
+        <?php } ?>
+        
     
 </body>
 </html>
